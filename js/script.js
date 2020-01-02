323195c4f2cca58221bce545e29dd487929645cb
Vale <vale@MacBook-Pro.homenet.telecomitalia.it>
2020-01-02 11:06:59 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function playE2() {
+    const e2 = document.getElementById("e2");
+    e2.play();
+}
+
+function playA2() {
+    const a2 = document.getElementById("a2");
+    a2.play();
+}
+
+function playD3() {
+    const d3 = document.getElementById("d3");
+    d3.play();
+}
+
+function playG3() {
+    const g3 = document.getElementById("g3");
+    g3.play();
+}
+
+function playB3() {
+    const b3 = document.getElementById("b3");
+    b3.play();
+}
+
+function playE4() {
+    const e4 = document.getElementById("e4");
+    e4.play();
+}
